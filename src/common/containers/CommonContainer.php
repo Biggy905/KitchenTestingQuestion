cfbdaa7717b5cff33f00f72cbcc8f0888dc6a43e
biggy905 <alexander-a-bolgov@yandex.ru>
2023-03-21 22:27:05 +0300

    feat: add DI-containers

--- src/common/containers/CommonContainer.php
+++ src/common/containers/CommonContainer.php
@@ -3,6 +3,10 @@
 namespace common\containers;
 
 use common\forms\AddBookForm;
+use common\forms\AuthForm;
+use common\forms\FilterForm;
+use common\forms\IdForm;
+use common\forms\RegistrationForm;
 use common\forms\TokenForm;
 use common\repositories\BookRepositoryInterface;
 use common\repositories\databases\BookRepositoriy;
@@ -10,7 +14,9 @@ use common\repositories\databases\TokenRepository;
 use common\repositories\databases\UserRepository;
 use common\repositories\TokenRepositoryInterface;
 use common\repositories\UserRepositoryInterface;
+use common\services\AuthentificationService;
 use common\services\BookService;
+use common\services\JwtService;
 use common\services\UserService;
 
 final class CommonContainer
@@ -24,9 +30,15 @@ final class CommonContainer
 
             TokenForm::class => TokenForm::class,
             AddBookForm::class => AddBookForm::class,
+            RegistrationForm::class => RegistrationForm::class,
+            AuthForm::class => AuthForm::class,
+            IdForm::class => IdForm::class,
+            FilterForm::class => FilterForm::class,
 
             BookService::class => BookService::class,
             UserService::class => UserService::class,
+            AuthentificationService::class => AuthentificationService::class,
+            JwtService::class => JwtService::class,
         ];
     }
 }
